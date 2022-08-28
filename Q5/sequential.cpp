348d5e16dd701b280f36f5c5a79879712f95b658
kjain1810 <kunaljain.2000@yahoo.in>
2022-08-28 16:58:24 +0530

    New logic for Q5

--- Q5/sequential.cpp
+++ Q5/sequential.cpp
@@ -4,15 +4,34 @@
 int main() {
   int n;
   std::cin >> n;
-  std::vector<std::vector<int>> adj(n, std::vector<int>(n, 0));
+  std::vector<std::vector<int>> A(n, std::vector<int>(n, 0));
   for (int a = 0; a < n; a++)
     for (int b = 0; b < n; b++)
-      std::cin >> adj[a][b];
+      std::cin >> A[a][b];
+
+  // B = A * A
+  std::vector<std::vector<int>> B(n, std::vector<int>(n, 0));
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      for (int k = 0; k < n; k++)
+        B[i][k] += A[i][j] * A[j][k];
+
+  // C = B * A
+  std::vector<std::vector<int>> C(n, std::vector<int>(n, 0));
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      for (int k = 0; k < n; k++)
+        C[i][k] += B[i][j] * A[j][k];
   int ans = 0;
+  for (int i = 0; i < n; i++)
+    ans += C[i][i];
+  std::cout << ans / 2 << std::endl;
+
+  ans = 0;
   for (int a = 0; a < n; a++)
     for (int b = a + 1; b < n; b++)
       for (int c = b + 1; c < n; c++)
-        if (adj[a][b] && adj[a][c] && adj[b][c])
+        if (A[a][b] && A[a][c] && A[b][c])
           ans++;
   std::cout << ans << std::endl;
   return 0;
